b167c14d2c2ad7853606441a7e9c4c6a7b07f2d8
Best1234GG <61874824+Best1234GG@users.noreply.github.com>
2023-12-27 23:51:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
-
-        if (days != 0 && hours != 0 && minutes != 0 && seconds != 0){
-            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-        }
         const button = document.getElementById("button");
         const text = document.getElementById("text");
-        if (/*days == 0 && hours == 0 && minutes == 0 &&*/ seconds == 0){
+        if (days == 0 && hours == 0 && minutes && seconds == 0){
             countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; 
             button.style.display = "inline-block";
-            text.style.marginLeft = "200px";
-            countdown.style.marginLeft = "200px";
+            text.style.marginLeft = "450px";
+            countdown.style.marginLeft = "450px";
 
+        }else{
+            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; 
         }
     }
 
