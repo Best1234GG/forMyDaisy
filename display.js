1096a5b10f17ce1f8ee3ff819c017967ad64ec60
Best1234GG <61874824+Best1234GG@users.noreply.github.com>
2023-12-29 13:21:22 +0700

    Update display.js

---
+++ display.js
@@ -0,0 +1,5 @@
+//<button class="button" id="scrollToTopBtn" data-scroll data-speed="800" data-easing="easeInOutCubic">Enter</button>
+var button = document.getElementById('scrollToTopBtn);
+if (button) {
+  windows.location = "love.html";                                     
+}
