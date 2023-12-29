d0e1f4c51f14fcc88b2d18bdeab8eb10e7f6b105
Best1234GG <61874824+Best1234GG@users.noreply.github.com>
2023-12-30 01:19:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     const countdownElement = document.getElementById('countdown');
-    
+    const video = document.getElementById('video');
+
     function updateCountdown() {
         const now = new Date();
         const newYear = new Date(now.getFullYear() + 1, 0, 1);
@@ -12,20 +13,20 @@ document.addEventListener('DOMContentLoaded', function () {
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
         const button = document.getElementById("button");
         const text = document.getElementById("text");
-        if (days == 0 && hours == 0 && minutes && seconds == 0){
+        if (/*days == 0 && hours == 0 && minutes && */seconds == 5){
             countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; 
             button.style.display = "inline-block";
-            text.style.marginLeft = "450px";
-            countdown.style.marginLeft = "450px";
+            text.style.marginLeft = "250px";
+            countdown.style.marginLeft = "250px";
 
         }else{
-            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; 
+            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        }
+        if (days == 0 && hours == 0 && minutes && seconds <= 10){
+            video.play();
         }
     }
 
-    // Update every second
     setInterval(updateCountdown, 1000);
-
-    // Initial call to set initial values
     updateCountdown();
 });
