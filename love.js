347e33601b1819672f3a479efa5c98301f8f848d
Best1234GG <61874824+Best1234GG@users.noreply.github.com>
2023-12-30 10:00:35 +0700

    Delete love.js

--- love.js
+++
@@ -1,8 +0,0 @@
-function clickbutton(){
-    var click = document.getElementById('button');
-    var text = document.getElementById('text')
-    if (click){
-        text.style.display = "inline-block";
-        click.remove();
-    }
-}
