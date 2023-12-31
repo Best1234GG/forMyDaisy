198c4f78d49690e659936e946b64156f2115a201
Best1234GG <61874824+Best1234GG@users.noreply.github.com>
2024-01-01 00:20:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -13,14 +13,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
         const button = document.getElementById("button");
         const text = document.getElementById("text");
-        if (days == 0 && hours == 0 && minutes == 0 && seconds == 0){
-            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`; 
-            button.style.display = "inline-block";
-            text.style.marginLeft = "250px";
-            countdown.style.marginLeft = "250px";
-
-        }else{
-            countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        countdownElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
         }
         if (days == 0 && hours == 0 && minutes == 0 && seconds <= 10){
             video.play();
